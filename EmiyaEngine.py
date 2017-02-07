7356d420c834943826fe018d0c91c3029c14ab77
Sg4Dylan <dengbill123@gmail.com>
2017-02-07 20:02:23 +0800

    init

---
+++ EmiyaEngine.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Emiya Engine
+# Version: Alpha.0 Rev.3
+# Author: Sg4Dylan - <sg4dylan#gmail.com>
+# Created: 02/07/2017
+# 真正重要的東西，只用眼睛是看不見的，
+# 只要蘊藏著想成為真物的意志，偽物就比真物還要來得真實
+
+import os
+import argparse
+import datetime
+import uuid
+import random
+import math
+import numpy as np
+import scipy
+import librosa
+import resampy
+from colorama import Fore, Back, init
+
+class EmiyaEngineCore:
+
+    ReadyFilePath = ''      # 输入文件名
+    BeforeSignal = ''       # 输入原始信号
+    BeforeSignalSR = 0      # 原始信号采样
+    MidSignal = ''          # 重采样到96K信号
+    MidSignalSR = 0         # 96K
+    AfterSignalLeft = ''    # 处理后的左信号
+    AfterSignalRight = ''   # 处理后的右信号
+    AfterSignal = ''        # 处理后的信号
+    AfterSignalSR = 0       # 处理后的采样率
+    SplitSize = 0           # 倒腾区大小
+    MidSRCFalse = False     # SRC开关, 为True时就会取消掉SRC步骤
+    MidPrint = False        # 打印细节日志开关
+    MidPrintProgress = True # 打印进度信息
+
+    def __init__(self, _InputFilePath, _DebugSwitch, _SplitSize):
+        # 输入样本:
+        # 信号被加载为ndarray结构，有两个声道各以一维array形式存在
+        self.ReadyFilePath = _InputFilePath
+        self.SplitSize = _SplitSize
+        if _DebugSwitch == 0:
+            MidPrintProgress = False
+        elif _DebugSwitch == 1:
+            MidPrintProgress = True
+        elif _DebugSwitch == 2:
+            MidPrint = True
+        else:
+            pass
+        self.LoadFile()
+        self.MidUpSRC()
+        self.ProcessCore()
+
+    def LoadFile(self):
+        self.BeforeSignal, self.BeforeSignalSR = librosa.load(self.ReadyFilePath, sr=None, mono=False)
+        self.AfterSignalLeft = np.array([()])
+        self.AfterSignalRight = np.array([()])
+        print("Load signal complete. ChannelCount: %s SampleRate: %s Hz" % (str(len(self.BeforeSignal)), str(self.BeforeSignalSR)))
+
+    def MidUpSRC(self):
+        # 重采样loss样本到96K
+        print("Please wait for SRC.")
+        self.MidSignalSR = 96000
+        self.AfterSignalSR = self.MidSignalSR
+        if self.MidSRCFalse:
+            self.MidSignal = self.BeforeSignal
+        else:
+            self.MidSignal = resampy.resample(self.BeforeSignal, self.BeforeSignalSR, self.MidSignalSR, filter='kaiser_best')
+        print("Signal SRC complete.")
+
+    def MidFindThresholdPoint(self,_MidFFTResultSingle,_FFTPointCount):
+        # 鉴定频谱基本参数
+        _MidAmpData = abs(_MidFFTResultSingle[range(_FFTPointCount//2)])
+        # Step0. 找出基波幅度
+        _MidBaseFreqAmp = _MidAmpData.max()
+        if self.MidPrint:
+            print("Signal max AMP -> %s" % _MidBaseFreqAmp)
+        # Step1. 找出接续的阈值
+        _MidThresholdHit = 1.0e-11                                     # 方差判定阈值
+        _MidThresholdPoint = 0                                         # 最后的阈值点
+        _MidFindRange = int((_FFTPointCount/2)-1)                      # 搜索的范围
+        _MidStartFindPos = round(2000/(48000/(_FFTPointCount/2)))      # 从2K频点附近开始寻找，加快速度
+        _MidStartFlag = True                                           # 循环用的启动Flag
+        _MidLoopCount = 0                                              # 循环计数器
+        _MidLegalFreq = 22000                                          # 判定结果合法的阈值频率
+        _MidForwardFreq = 3000                                         # 前向修正频率
+        _MidOrderFreq = 16000                                          # 钦定频率
+        # Rev.1: 检查接续点是否符合常理
+        while _MidStartFlag or _MidThresholdPoint>round(_MidLegalFreq/(48000/(_FFTPointCount/2))):
+            _MidStartFlag = False
+            if (_MidThresholdPoint*(48000/(_FFTPointCount/2))) > int(self.BeforeSignalSR/2):
+                _MidThresholdHit *= 2
+            for i in range(_MidStartFindPos,_MidFindRange):
+                if i+5>_MidFindRange:
+                    break
+                # 计算连续五个采样*3 的方差，与阈值比较，判断频谱消失的位置
+                if np.var(_MidAmpData[i:i+4]) < _MidThresholdHit and \
+                   np.var(_MidAmpData[i+1:i+5]) < _MidThresholdHit:
+                    # 定位到当前位置的前500Hz位置
+                    _MidThresholdPoint = i-round(_MidForwardFreq/(48000/(_FFTPointCount/2)))
+                    break
+            # 错误超过5把就强行钦定频率为18K
+            _MidLoopCount += 1
+            if _MidLoopCount > 5:
+                _MidThresholdPoint = round(_MidOrderFreq/(48000/(_FFTPointCount/2)))
+                break
+        # 打印函数返回信息
+        if self.MidPrint:
+            print("Signal threshold point -> %s @ %sHz  Max Amp -> %s" % (_MidThresholdPoint,_MidThresholdPoint*(48000/(_MidFindRange+1)),_MidBaseFreqAmp))
+        # _MidThresholdPoint = round(21000/(48000/(_FFTPointCount/2)))
+        return _MidBaseFreqAmp, _MidThresholdPoint
+
+    def MidInsertJitter(self,_MidFFTResultDouble,_FFTPointCount,_MidThresholdPoint,_MidBaseFreqAmp):
+        # 构造抖动
+        if _MidThresholdPoint <= 0:
+            return _MidFFTResultDouble
+        for i in range(_MidThresholdPoint,_FFTPointCount-_MidThresholdPoint):
+            # Rev.0: 调整生成概率，频率越高概率越低
+            # Rev.1: 加入幅值判定，幅度越大概率越大
+            _GenPossible = abs((_FFTPointCount/2)-i)/((_FFTPointCount/2)-_MidThresholdPoint)*(_MidBaseFreqAmp/0.22)
+            if random.randint(0, 1000000) < 800000 * _GenPossible: # 0<=x<=10
+                _MidRealValue = abs(_MidFFTResultDouble.real[i])
+                _BaseJitterMin = _MidRealValue * 0.5 * (1-_GenPossible)
+                _BaseJitterMax = _MidRealValue * 6 * _GenPossible
+                _AmpJitterMin = _MidBaseFreqAmp * _MidRealValue * 0.5
+                _AmpJitterMax = _MidBaseFreqAmp * _MidRealValue * 2
+                _AmpJitterPrefix = -1 if random.randint(0, 100000) < 50000 else 1
+                _MiditterPrefix = -1 if random.randint(0, 100000) < 50000 else 1
+                _MidDeltaJitterValue = random.uniform(_BaseJitterMin,_BaseJitterMax) + _AmpJitterPrefix * random.uniform(_AmpJitterMin,_AmpJitterMax)
+                _MidFFTResultDouble.real[i] += _MiditterPrefix * _MidDeltaJitterValue
+        return _MidFFTResultDouble
+
+    def FinSaveFile(self):
+        init(autoreset=True)
+        OutputFilePath = os.path.abspath(os.path.join(self.ReadyFilePath, os.pardir)) + "\\"
+        OutputFileName = OutputFilePath + 'Output_%s.wav' % uuid.uuid4().hex
+        librosa.output.write_wav(OutputFileName, self.AfterSignal, self.AfterSignalSR)
+        print(Back.GREEN + Fore.WHITE + "SAVE DONE" + Back.BLACK + " Output path -> " + OutputFileName)
+
+    def ProcessCore(self):
+        # 初始化彩色命令行
+        init(autoreset=True)
+        # 两个声道
+        for ChannelIndex in range(2):
+            # 记录开始时间
+            _MidStartTime = datetime.datetime.now()
+            # 信号总长度
+            _MidSignalLength = len(self.MidSignal[ChannelIndex])
+            # FFT分割数量
+            _FFTPointCount = 1024 # 至少2048点，避免计算错误
+            _MidDivCount = math.floor(_MidSignalLength/_FFTPointCount)
+            # 实际重叠操作数量 = FFT分割数量 * 分块次数
+            _EachLength = 512
+            # 补偿标记, 标记有效时, 说明已经到了序列尾部, 因停止继续循环运算
+            SuffixFlag = False
+            SuffixLength = 0
+            # Rev.2: 加入临时数组加速Append, 临时数组每Append操作设定次数就倒腾一次
+            _TempArrayLeft = np.array([()])
+            _TempArrayRight = np.array([()])
+            _TempAppendCount = 0
+            # 除了最后一块，每一块都是取计算结果时域的前512点
+            for SamplePointIndex in range(_MidDivCount+1):
+                StartPos = SamplePointIndex*_FFTPointCount
+                EndPos = SamplePointIndex*_FFTPointCount+_FFTPointCount
+                _EachPieceLeft = np.array([()])
+                _EachPieceRight = np.array([()])
+                for EachFourPiece in range(int(_FFTPointCount/_EachLength)):
+                    StartPos += EachFourPiece * _EachLength
+                    EndPos += EachFourPiece * _EachLength
+                    # 若超出范围, 需将本次计算完整保留接续有效部分(除去补零部分)
+                    if EndPos > _MidSignalLength:
+                        EndPos = _MidSignalLength
+                        SuffixFlag = True
+                    _TempSignal = self.MidSignal[ChannelIndex][StartPos:EndPos]
+                    # 不足FFT点数的补零
+                    while len(_TempSignal) != _FFTPointCount:
+                        _TempSignal = np.append(_TempSignal, [0])
+                        SuffixLength += 1
+                    # 执行FFT运算, 单边谱用于分析, 双边谱用于处理
+                    _MidFFTResultDouble = np.fft.fft(_TempSignal,_FFTPointCount)/(_FFTPointCount)
+                    _MidFFTResultSingle = np.fft.fft(_TempSignal*scipy.signal.hann(_FFTPointCount, sym=0),_FFTPointCount)/(_FFTPointCount/2)
+                    # 获取当前分段最大振幅, 处理阈值点
+                    _MidBaseFreqAmp, _MidThresholdPoint = self.MidFindThresholdPoint(_MidFFTResultSingle,_FFTPointCount)
+                    # 构造抖动到当前FFT实际值上
+                    _MidFFTAfterJitter = self.MidInsertJitter(_MidFFTResultDouble,_FFTPointCount,_MidThresholdPoint,_MidBaseFreqAmp)
+                    # 逆变换IFFT
+                    _MidTimerDomSignal = np.fft.ifft(_MidFFTAfterJitter,n=_FFTPointCount)
+                    # 接续到新信号上
+                    _AppendLength = _EachLength
+                    if SuffixFlag:
+                        _AppendLength = _FFTPointCount-SuffixLength
+                    _MidAppendSignal = _MidTimerDomSignal[0:_AppendLength]
+                    if self.MidPrint:
+                        print("Per each length -> %s" % len(_MidAppendSignal))
+                    if ChannelIndex == 0:
+                        _EachPieceLeft = np.append(_EachPieceLeft, _MidAppendSignal)
+                    else:
+                        _EachPieceRight = np.append(_EachPieceRight, _MidAppendSignal)
+                    # 及时跳出尾部
+                    if SuffixFlag:
+                        break
+                # 先倒腾到临时数组，倒腾500次给放回大数组
+                if _TempAppendCount < self.SplitSize and not SuffixFlag:
+                    # 已消耗时间
+                    _MidUsedTime = datetime.datetime.now()-_MidStartTime
+                    # 估算剩余时间
+                    _MidEtaTime = ((datetime.datetime.now()-_MidStartTime)/((1 if SamplePointIndex==0 else SamplePointIndex)/_MidDivCount))-_MidUsedTime
+                    # 构造显示文本
+                    if ChannelIndex == 0:
+                        _TempArrayLeft = np.append(_TempArrayLeft, _EachPieceLeft)
+                        if self.MidPrintProgress:
+                            print("Left channel progress rate -> " + Fore.CYAN + str(SamplePointIndex) + " / " + str(_MidDivCount) + Fore.WHITE + " TIME USED -> " + Fore.YELLOW + str(_MidUsedTime) + Fore.WHITE + " ETA -> " + Fore.GREEN + str(_MidEtaTime))
+                    else:
+                        _TempArrayRight = np.append(_TempArrayRight, _EachPieceRight)
+                        if self.MidPrintProgress:
+                            print("Right channel progress rate -> " + Fore.CYAN + str(SamplePointIndex) + " / " + str(_MidDivCount) + Fore.WHITE + " TIME USED -> " + Fore.YELLOW + str(_MidUsedTime) + Fore.WHITE + " ETA -> " + Fore.GREEN + str(_MidEtaTime))
+                else:
+                    _TempAppendCount = 0
+                    if ChannelIndex == 0:
+                        self.AfterSignalLeft = np.append(self.AfterSignalLeft, _TempArrayLeft)
+                        _TempArrayLeft = np.array([()])
+                    else:
+                        self.AfterSignalRight = np.append(self.AfterSignalRight, _TempArrayRight)
+                        _TempArrayRight = np.array([()])
+                # 倒腾计数器
+                _TempAppendCount += 1
+                if SuffixFlag:
+                    break
+        self.AfterSignal = np.array([self.AfterSignalLeft.real, self.AfterSignalRight.real])
+        self.FinSaveFile()
+
+
+def Main(_input,_debug,_size):
+    Processor = EmiyaEngineCore(_input,_debug,_size)
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+                                     description='Emiya Engine\n'
+                                                 'Version: Alpha.0 Rev.2\n'
+                                                 'Author: Sg4Dylan - <sg4dylan#gmail.com>\n'
+                                                 '真正重要的東西, 只用眼睛是看不見的, \n'
+                                                 '只要蘊藏著想成為真物的意志, 偽物就比真物還要來得真實.')
+    parser.add_argument('-i', '--input', help='待处理文件的绝对路径, 同一路径可直接输入文件名. 例如: \n'
+                                            'Music_ready_test.mp3')
+    parser.add_argument('-d', '--debug', help='调试等级设定. 默认 1 级. \n'
+                                            '设置为 0 时, 只显示任务起始日志; \n'
+                                            '设置为 1 时, 额外显示进度日志; \n'
+                                            '设置为 2 时, 额外显示处理细节日志')
+    parser.add_argument('-s', '--size', help='倒腾区大小. 默认 500. \n'
+                                            '使用倒腾区是因为 numpy 做大数组 append 速度远低于小数组, \n'
+                                            '故加入小数组多倒腾一手, 这个参数就是小数组的尺寸.')
+
+    args = parser.parse_args()
+    _input = args.input
+    _debug = args.debug
+    _size = args.size
+    
+    if not _input:
+        print("缺少输入文件参数，请使用 --help 参考!")
+        exit(1)
+    if not _debug:
+        _debug = 1
+    if not _size:
+        _size = 500
+    
+    Main(_input,_debug,_size)
+
--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# EmiyaEngine
-A script for enriching audio spectrum.
+# Emiya Engine 
+
+> "真正重要的東西，只用眼睛是看不見的。
+> 只要蘊藏著想成為真物的意志，偽物就比真物還要來得真實。"
+
+Emiya Engine 是一个用来丰富音频频谱的脚本。可以将频谱变得好看那么一点。
+原理是使用 FFT (快速傅立叶变换) 将音频信号采样转到频域，在频域上为空白的频谱加上与时域幅值相称的微小抖动。
+
+###使用须知：
+
+ - 由于 FFT 的栅栏效应，程序的处理过程不可避免地会损失部分采样信号。故不建议将本程序用于玄学领域。
+ -  由于程序缺乏细致调教，当前算法会导致部分采样块未被有效处理。
+ -  由于玄学原因，最后生成的文件有一定几率出现时长一百余个采样点（0.0016s）的爆音区域。
+ -  由于处理需要消耗大约是音源时长的 2 - 10 倍的时间，故不建议输入较长的音频文件。
+ -  鉴于脚本输出为 96KHz 采样 32bit 单精度浮点型 的 WAV 文件，请不要输入比输出精度更高的文件。
+
+###程序依赖：
+
+ - Python 3
+ - numpy
+ - scipy
+ - librosa
+ - resampy
+ - colorama
+
+####依赖安装建议
+Windows平台：
+
+> 实际上 librosa 的依赖非常多，如果使用 `pip`安装，可能会导致出错，
+> 建议直接在[这里](http://www.lfd.uci.edu/~gohlke/pythonlibs/)下载二进制包使用`pip`离线安装。
+> 除了以上列表的依赖，还有`Cython` 和 `scikit-learn`，建议一块装了。
+> `librosa` 需要配置 `ffmpeg` 的目录，
+> 找到 `Python` 安装目录下 `Lib\site-packages\audioread` 文件夹的 `ffdec.py` 文件。
+> 修改第 32 行，修改为你的 `ffmpeg` 程序路径，比如我的放在 F 盘根目录，设置成这样：
+> `COMMANDS = ('F:\\ffmpeg.exe', 'avconv')`
+
+Linux平台：
+
+> ArchLinux 上建议用 pacman 一路搞定 numpy scipy scikit-learn Cython，当然使用pip也是OK的。 
+> Debian/Ubuntu 上默认源似乎必须用 apt-get 安装，有一些包用 pip 安装有些问题。 
+> 与 Windows 平台一样，也需要为 librosa安装后端解码音频文件，
+> 直接使用发行版自带的包管理器安装 ffmpeg 就可以。
+> 如果出错了，建议从源编译最新的放在原来的路径下。
+
+###命令行帮助：
+
+    -h, --help          显示帮助信息.
+    -i INPUT, --input INPUT
+                        待处理文件的绝对路径, 同一路径可直接输入文件名. 例如:
+                        Music_ready_test.mp3
+    -d DEBUG, --debug DEBUG
+                        调试等级设定. 默认 1 级.
+                        设置为 0 时, 只显示任务起始日志;
+                        设置为 1 时, 额外显示进度日志;
+                        设置为 2 时, 额外显示处理细节日志
+    -s SIZE, --size SIZE  倒腾区大小. 默认 500.
+                        使用倒腾区是因为 numpy 做大数组 append 速度远低于小数组,
+                        故加入小数组多倒腾一手, 这个参数就是小数组的尺寸.
+
+###效果预览：
+音源：44.1KHz@16bit WAV
+![enter image description here](https://i.imgur.com/VU9Obqw.jpg)
+
